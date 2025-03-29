dbda897d1e21f59db7090127fe3837251089b766
annamaggg <annamagnusson99@gmail.com>
2025-03-29 10:41:26 +0000

    add orbit controls, lighting, plane with rendered sample image/map

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,34 @@
-import { Canvas } from "@react-three/fiber";
+import React, { StrictMode, useRef, Suspense} from 'react'
+import { Canvas, useLoader } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import { TextureLoader } from 'three'
 
 export default function App() {
   return (
     <>
-    <Canvas camera={{ position: [0, 0, 3] }} style={{ width: '100vw', height: '100vh' }}>
-      <mesh position={[0, 0, 0]}>
-        <planeGeometry args={[1, 1]} />
-        <meshBasicMaterial color="hotpink" />
-      </mesh>
+    <Canvas camera={{ position: [0, 0, 3] }} style={{ width: '100vw', height: '100vh' }} >
+      <OrbitControls /> 
+      <ambientLight intensity={0.2} /> 
+      <directionalLight position={[-5, 5, 5]} intensity={0.2} color="blue" castShadow />
+      <directionalLight position={[5, -5, 5]} intensity={0.2} color="red" />
+      <directionalLight position={[2, 5, 3]} intensity={4} />
+      <PlaneWithTexture />
     </Canvas>
     </>
+  );
+}
+
+function PlaneWithTexture() {
+  const meshRef = useRef()
+  const materialRef = useRef()
+
+  const texture = useLoader(TextureLoader, '/images/sample-image.png')
+  const displacementMap = useLoader(TextureLoader, '/images/sample-map.webp')
+
+  return (
+    <mesh ref={meshRef} castShadow={true} receiveShadow={true}>
+      <planeGeometry args={[20, 20, 364, 364]} />
+      <meshStandardMaterial ref={materialRef}  wireframe={false} map={texture} displacementMap={displacementMap} displacementScale={2}  roughness={0.1}  />
+    </mesh>
   );
 }
