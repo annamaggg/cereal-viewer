2cd4733c35c748b73b77b7967a5ddb6870737b12
annamaggg <annamagnusson99@gmail.com>
2025-03-29 13:46:49 +0000

    add gradient background

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Canvas, useLoader } from "@react-three/fiber";
 import { OrbitControls, useFBX } from "@react-three/drei";
 import { TextureLoader } from 'three'
 import Form from './components/Form';
+import * as THREE from "three";
 
 export default function App() {
   const [textureUrl, setTextureUrl] = useState('/images/sample-image.png');
@@ -23,6 +24,7 @@ export default function App() {
     <Form onTextureFileUpload={handleTextureFileUpload} onDepthMapFileUpload={handleDepthMapFileUpload} />
     <Canvas camera={{ position: [0, 0, 7] }} style={{ width: '100vw', height: '100vh' }} >
       <OrbitControls /> 
+      <Background />
       <ambientLight intensity={0.2} /> 
       <directionalLight position={[-5, 5, 5]} intensity={0.2} color="blue" castShadow />
       <directionalLight position={[5, -5, 5]} intensity={0.2} color="red" />
@@ -72,3 +74,38 @@ const CerealBox = () => {
 
   return <primitive object={fbx} scale={0.035} position={[0, -3, 0]}></primitive>;
 };
+
+const vertexShader = `
+  varying vec2 vUv;
+  void main() {
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+  }
+`;
+
+const fragmentShader = `
+  varying vec2 vUv;
+  void main() {
+    float gradient = 0.5 + 0.5 * cos(vUv.y * 3.14159265359); // Smooth cycle
+    vec3 colorB = vec3(0.9, 0.8, 0.7);
+    vec3 colorA = vec3(0.8, 0.6, 0.9); 
+    vec3 finalColor = mix(colorA, colorB, gradient);
+    gl_FragColor = vec4(finalColor, 1.0);
+  }
+`;
+
+function Background() {
+  return (
+    <mesh scale={50}>
+      <sphereGeometry args={[1, 64, 64]} />
+      <shaderMaterial
+        args={[{
+          vertexShader,
+          fragmentShader,
+          uniforms: {},
+          side: THREE.BackSide
+        }]}
+      />
+    </mesh>
+  );
+}
